df87b5211ea8f1c6361cecb8d7dbec1f49e8eacc
Isaac Tretta <isaac.tretta@ucr.ac.cr>
2019-06-27 14:46:13 -0600

    Modificado MailRetriever, SpamFilter.

--- src/main/java/SpamFilter.java
+++ src/main/java/SpamFilter.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Map;
 
 public class SpamFilter {
@@ -5,4 +6,26 @@ public class SpamFilter {
     private Map<String,Term> blackList;
     private FileManager fileManager;
     private FilterConfiguration filterConfiguration;
+
+    public void isSpam(ArrayList<Email> newEmails, double probabilitySpam, double spamThreshold){
+        double productProbSpam = 1;
+        double productProbNotSpam = 1;
+        double probabilityNotSpam = (double)(1-probabilitySpam);
+        double probEmailSpam;
+        for (Email email: newEmails){
+            String[] words = email.getBody().split(" ");
+            for (String word: words){
+                if(blackList.containsKey(word) && blackList.get(word).getFrequencyNormal() > 0 &&
+                        blackList.get(word).getFrequencySpam() > 0){
+                    productProbSpam *= blackList.get(word).getProbSpam() * probabilitySpam;
+                    productProbNotSpam *= blackList.get(word).getProbNormal() * probabilityNotSpam;
+                }
+            }
+            probEmailSpam = productProbSpam / (productProbSpam + productProbNotSpam);
+            if(probEmailSpam >= spamThreshold)
+                System.out.printf("- %s\n", "Snippet: " + email.getSubject() + "\n" + "This Email Is SPAM.");
+            else
+                System.out.printf("- %s\n", "Snippet: " + email.getSubject() + "\n" + "This Email Is NOT SPAM :)");
+        }
+    }
 }
